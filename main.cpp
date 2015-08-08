b86476dc1f306db4c8de934706dbfd7ca9fb3817
Demetri0 <d3i0@ya.ru>
2015-08-09 01:38:45 +0600

    Change default settings

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@
 #include <QDir>
 #include <QSettings>
 
+QUrl makeStartUrl(QString htmlDir, QString startPage)
+{
+    QString url;
+    url += QDir::currentPath()
+        +  QDir::separator()
+        +  htmlDir
+        +  QDir::separator()
+        +  startPage;
+
+    return QUrl::fromLocalFile( url );
+}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -16,28 +28,23 @@ int main(int argc, char *argv[])
 
         app.setApplicationVersion( cfg.value("app_version", "0.0.0").toString() );
 
-        QString htmlDir   = cfg.value("html_dir", "html").toString();
-        QString startPage = cfg.value("start_page", "index.html").toString();
+        QString htmlDir   = cfg.value("html_dir", "html/Anime/UI/White/").toString();
+        QString startPage = cfg.value("start_page", "Main.html").toString();
 
-        QString url;
-        url += QDir::currentPath()
-            +  QDir::separator()
-            +  htmlDir
-            +  QDir::separator()
-            +  startPage;
-
-        view->load( QUrl::fromLocalFile( url ) );
+        view->load( makeStartUrl(htmlDir, startPage) );
 
         QString window_icon = QDir::currentPath()
                             +  QDir::separator();
         QIcon icon( window_icon + cfg.value("window_icon", "icon.png").toString() );
         view->setWindowIcon( icon );
         view->setWindowTitle( cfg.value("window_title", "AniTime").toString() );
+
         Qt::ContextMenuPolicy context_menu;
         context_menu = ( cfg.value("context_menu", true).toBool() )
                        ? Qt::DefaultContextMenu
                        : Qt::NoContextMenu;
         view->setContextMenuPolicy( context_menu );
+
     }
 
     view->show();
